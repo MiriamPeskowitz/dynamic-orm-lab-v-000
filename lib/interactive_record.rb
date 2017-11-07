5f99f17b179755b4e811ff50a05608b20c603553
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-07 19:18:54 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -66,8 +66,8 @@ class InteractiveRecord
       DB[:conn].execute(sql)
     end
 
-
-    def self.find_by(attribute_hash)
+# use #values 
+    def self.find_by(attr_hash)
       value = attr_hash.values.first
       formatted_value = value.class == Fixnum ? value : "'#{value}'"
       sql = "SELECT * FROM #{self.table_name} WHERE #{attr_hash.keys.first} = #{formatted_value}"
@@ -85,26 +85,3 @@ class InteractiveRecord
     end
 # handle different keys -- and different types of values, int and text Access a key without typing the name of the key .first
 make string or use logic
-#
-#     self.id = attr_hash[:id]
-#     self.name = attr_hash[:name]
-#     self.grade = attr_hash[:grade]
-#
-# maybe try a reify_from_row? -- no, that's for a row/values only , not a hash
-#
-#   self.reify_from_row(row)
-#     self.new.tap do |o|
-#       o.id = row[0]
-#       o.name = row[1] etc
-#
-#       Spec file
-#     it 'executes the SQL to find a row by the attribute passed into the method' do
-#       Student.new({name: "Susan", grade: 10}).save
-#       expect(Student.find_by({name: "Susan"})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10, 0=>1, 1=>"Susan", 2=>10}])
-#     end
-#
-#     it 'accounts for when an attribute value is an integer' do
-#       Student.new({name: "Susan", grade: 10}).save
-#       expect(Student.find_by({grade: 10})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10, 0=>1, 1=>"Susan", 2=>10}])
-#     end
-end
